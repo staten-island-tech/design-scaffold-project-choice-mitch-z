18c9adf0f381812e470a5846467d37c4b5c17ec7
mitch7zale <90780682+mitch7zale@users.noreply.github.com>
2022-05-25 17:54:26 -0400

    1

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,8 +1,9 @@
 module.exports = function(eleventyconfig){
     eleventyconfig.addPassthroughCopy("src/style");
-    eleventyconfig.addPassthroughCopy("src/Blog.html")
+    eleventyconfig.addPassthroughCopy("src/index.html")
     eleventyconfig.addPassthroughCopy("src/Blog.js");
     eleventyconfig.addPassthroughCopy("src/media")
+    eleventyconfig.addPassthroughCopy("src/Review1.html");
     return{
         dir:{
             input: "src",
