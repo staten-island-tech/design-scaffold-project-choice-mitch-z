97754de7dff9a652ac0f74f6007201610693908b
mitch7zale <90780682+mitch7zale@users.noreply.github.com>
2022-05-25 21:04:12 -0400

    final

--- src/src/Blog.js
+++ src/src/Blog.js
@@ -1,9 +1,9 @@
-const navSlide= () => {
+const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click', () =>{
+    burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
         
         navLinks.forEach((link, index) => {
